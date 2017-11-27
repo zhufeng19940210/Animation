48a9de866b9883d725e0886dc18e16db8fe8f34f
zhufeng <358244144@qq.com>
2017-11-27 15:50:30 -0800

    提价项目了哈哈

---
+++ CoreAnimtaion/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoreAnimtaion
+//
+//  Created by bailing on 2017/11/27.
+//  Copyright © 2017年 bailing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CoreAnimtaion/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CoreAnimtaion
+//
+//  Created by bailing on 2017/11/27.
+//  Copyright © 2017年 bailing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
